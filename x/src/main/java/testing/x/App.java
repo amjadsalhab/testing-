5d9c5cbd83f6e6df64dbb7054a80d59cbd9f9730
amjad <adss.com>
2020-02-22 20:39:12 +0200

    first commit

---
+++ x/src/main/java/testing/x/App.java
@@ -0,0 +1,13 @@
+package testing.x;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ x/src/main/java/testing/x/test.java
@@ -0,0 +1,16 @@
+package testing.x;
+
+public class test {
+
+	
+	
+	public double mul(double x ,double y) 
+	{
+		return 0;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
