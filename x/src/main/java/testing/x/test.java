a696f8a62bc09d563f986ef17d377da0e126de41
amjad <adss.com>
2020-02-26 20:42:03 +0200

    add function

--- x/src/main/java/testing/x/test.java
+++ x/src/main/java/testing/x/test.java
@@ -2,11 +2,11 @@ package testing.x;
 
 public class test {
 
-	
-	
-	public double mul(double x ,double y) 
+
+
+	public double add (double x ,double y)
 	{
-		return 0;
+		return x+y;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
